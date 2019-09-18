83385cf29b6475317a85fee5c1c27bd89194adcb
MikeJollie2707 <52678633+MikeJollie2707@users.noreply.github.com>
2019-09-17 17:51:42 -0700

    Replaced if else with switch case

--- Public Projects/Workshops/Workshop9_17_19/Controllable Shape/keyControlled.js
+++ Public Projects/Workshops/Workshop9_17_19/Controllable Shape/keyControlled.js
@@ -10,48 +10,44 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 
-document.addEventListener('keydown', function(e) {
-    
-  // left arrow key
-  if (e.which === 37) {
-    shape.dx = -1;
-    shape.dy = 0;
-  }
-  // up arrow key
-  else if (e.which === 38) {
-    shape.dy = -1;
-    shape.dx = 0;
-  }
-  // right arrow key
-  else if (e.which === 39) {
-    shape.dx = 1;
-    shape.dy = 0;
-  }
-  // down arrow key
-  else if (e.which === 40) {
-    shape.dy = 1;
-    shape.dx = 0;
-  }
-  
-  // a key
-  if (e.which === 65) {
-    shape.dx = -1;
-    shape.dy = 0;
-  }
-  // w key
-  else if (e.which === 87) {
-    shape.dy = -1;
-    shape.dx = 0;
-  }
-  // d key
-  else if (e.which === 68) {
-    shape.dx = 1;
-    shape.dy = 0;
-  }
-  // s key
-  else if (e.which === 83) {
-    shape.dy = 1;
-    shape.dx = 0;
+document.addEventListener('keydown', function(keyPressed) {
+  switch (keyPressed.which)
+  {
+    case 37: // left arrow key
+    {}
+    case 65: // 'a' key
+    {
+      shape.dx = -1;
+      shape.dy = 0;
+      break;
+    }
+
+    case 38: // up arrow key
+    {}
+    case 87: // 'w' key
+    {
+      shape.dx = 0;
+      shape.dy = -1;
+      break;
+    }
+
+    case 39: // down arrow key
+    {}
+    case 68: // 's' key
+    {
+      shape.dx = 1;
+      shape.dy = 0;
+      break;
+    }
+
+    case 40: // right arrow key
+    {}
+    case 83: // 'd' key
+    {
+      shape.dx = 0;
+      shape.dy = 1;
+      break;
+    }
   }
 });
 
@@ -59,7 +55,7 @@ function loop() {
   requestAnimationFrame(loop);
   
 
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   
   shape.x = shape.x + shape.dx;
   shape.y = shape.y + shape.dy;
@@ -70,7 +66,7 @@ function loop() {
 
 function drawShape(){
 	ctx.fillStyle = 'red';
-	ctx.fillRect(shape.x,shape.y,10,10);
+	ctx.fillRect(shape.x, shape.y, 10, 10);
 }
 
 requestAnimationFrame(loop);
